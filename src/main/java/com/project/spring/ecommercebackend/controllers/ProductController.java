d7093beef1d73c70e37e3beeebf9d27712ac10dd
Vamsi Krishna <vamsipvk2203@gmail.com>
2024-09-12 00:03:25 +0530

    Added Response Entities for all the Controller Responses

--- src/main/java/com/project/spring/ecommercebackend/controllers/ProductController.java
+++ src/main/java/com/project/spring/ecommercebackend/controllers/ProductController.java
@@ -4,8 +4,11 @@ import com.project.spring.ecommercebackend.dtos.FakeStoreResponseDTO;
 import com.project.spring.ecommercebackend.models.Product;
 import com.project.spring.ecommercebackend.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
@@ -22,14 +25,15 @@ public class ProductController {
 
     // localhost:8080/products/1
     @GetMapping("/{id}")
-    public Product getSingleProduct(@PathVariable("id") Long id) {
-        return productService.getSingleProduct(id);
+    public ResponseEntity<Product> getSingleProduct(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(productService.getSingleProduct(id), HttpStatus.OK);
     }
 
     // localhost:8080/products
     @GetMapping()
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+
     }
 
     // localhost:8080/products --> Response same as the Fakestore one.
@@ -39,26 +43,28 @@ public class ProductController {
 //    }
 
     @PostMapping()
-    public Product addProduct(@RequestBody Product product) {
-        return productService.addProduct(product);
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.addProduct(product), HttpStatus.OK);
+
     }
 
     // localhost:8080/products/1
     @PatchMapping("/{id}")
-    public Product updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
-        return productService.updateProduct(id, product);
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
+        return new ResponseEntity<>(productService.updateProduct(id, product), HttpStatus.OK);
+
     }
 
     // localhost:8080/products/1
     @PutMapping("/{id}")
-    public Product replaceProduct(@PathVariable("id") Long id, @RequestBody Product product) {
-        return productService.replaceProduct(id, product);
+    public ResponseEntity<Product> replaceProduct(@PathVariable("id") Long id, @RequestBody Product product) {
+        return new ResponseEntity<>(productService.replaceProduct(id, product), HttpStatus.OK);
     }
 
     // localhost:8080/products/1
     @DeleteMapping("/{id}")
-    public Product deleteProduct(@PathVariable("id") Long id) {
-        return productService.deleteProduct(id);
+    public ResponseEntity<Product> deleteProduct(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(productService.deleteProduct(id), HttpStatus.OK);
     }
 
 }
